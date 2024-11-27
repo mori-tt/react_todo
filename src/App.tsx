a4efc14e88ddd31adb096355d24b28c880bd5113
T_Mori <morit0301@gmail.com>
2024-11-27 20:44:02 +0900

    Implementing task addition and deletion functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todoText, setTodoText] = useState<string>("");
   const [incompleteTodos, setIncompleteTodos] = useState<string[]>([
     "TODO1",
     "TODO2",
@@ -10,21 +11,44 @@ function App() {
     "TODO1 完了",
     "TODO2 完了",
   ]);
+  const onChangeTodoText = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onClickDelete = (index: number) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
   return (
     <>
       <div className="input-area">
-        <input type="text" placeholder="TODOを入力" id="add-text" />
-        <button id="add-button">追加</button>
+        <input
+          type="text"
+          placeholder="TODOを入力"
+          id="add-text"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button id="add-button" onClick={onClickAdd}>
+          追加
+        </button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul id="incomplete-list">
-          {incompleteTodos.map((todo) => (
+          {incompleteTodos.map((todo, index) => (
             <li key={todo}>
               <div className="list-row">
                 <p className="todo-item">{todo}</p>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             </li>
           ))}
